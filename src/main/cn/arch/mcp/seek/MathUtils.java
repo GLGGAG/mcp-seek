46c71aecda954c1c5de989d42dc400e81bfec484
ag.wan <ag.wan@huolala.cn>
2025-03-04 15:57:18 +0800

    init

---
+++ src/main/cn/arch/mcp/seek/MathUtils.java
@@ -0,0 +1,9 @@
+package cn.arch.mcp.seek;
+
+public class MathUtils {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/main/cn/arch/mcp/seek/McpSeekApplication.java
@@ -0,0 +1,13 @@
+package cn.arch.mcp.seek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpSeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpSeekApplication.class, args);
+    }
+
+}
